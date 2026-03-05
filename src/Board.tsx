3a0c832475d1e71218cb2858f7c01246c04f92c6
Christian Schüler <sc.schueler@gmail.com>
2026-03-05 20:59:02 +0100

    Update win conditions

--- src/Board.tsx
+++ src/Board.tsx
@@ -3,6 +3,19 @@ import "./Board.css";
 
 export default function Board() {
   const [activePlayer, setActivePlayer] = useState((): string => "X");
+  let playerX: number[] = [];
+  let playerO: number[] = [];
+
+  const winningComibinations: number[][] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
   function setActivePlayerNext(): void {
     setActivePlayer((prev): string => (prev === "X" ? "O" : "X"));
@@ -12,7 +25,31 @@ export default function Board() {
     const cell = e.target as HTMLDivElement;
     if (cell.textContent === "") {
       cell.textContent = activePlayer;
+      if (activePlayer === "X") {
+        playerX = [...playerX, parseInt(cell.dataset.key)];
+        playerX.sort((a, b): number => a - b);
+      } else {
+        playerO = [...playerO, parseInt(cell.dataset.key)]);
+        playerO.sort((a, b): number => a - b);
+      }
       setActivePlayerNext();
+console.log(playerX);
+console.log(playerO);
+      // calculate if winning condition is met
+      // check only, if minum 3 moves of the current player have been made
+      if ((playerX.length >= 3)
+          || (playerO.length >= 3)) {
+        let hasWon = false;
+        if (activePlayer === "X") {
+          hasWon = winningComibinations.some((combination): boolean => combination.every((num): boolean => playerX.includes(num)));
+        } else {
+          hasWon = winningComibinations.some((combination): boolean => combination.every((num): boolean => playerO.includes(num)));
+        }
+        if (hasWon) {
+          alert(`Player ${activePlayer} wins!`);
+          resetGame();
+        }
+      }
     }
   }
 
@@ -21,6 +58,8 @@ export default function Board() {
     cells.forEach((cell): void => {
       cell.textContent = "";
     });
+    setPlayerX([]);
+    setPlayerO([]);
     setActivePlayer("X");
   }
 
@@ -30,7 +69,7 @@ export default function Board() {
       <div className="action"><button type="button" onClick={resetGame}>Reset Game</button></div>
       <div className="board" onClick={cellClicked}>
         {[...Array(9)].map((_, i): JSX.Element => (
-          <div key={i} className="cell" />
+          <div key={i} data-key={i} className="cell" />
         ))}
       </div>
     </>
